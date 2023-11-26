590c7d0d4baf45b51f1f7a2d2a47af7c8f95f60a
bre <iwanttosignup321@gmail.com>
2023-11-26 15:35:36 +0800

    grid is bad

--- components/CommentV1.vue
+++ components/CommentV1.vue
@@ -36,7 +36,8 @@ const showTime = computed(() => {
 <template>
   <link rel="preconnect" href="https://rsms.me/" />
   <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-  <div v-if="comment" class="comment">
+  <div v-if="comment" class="comment ">
+    <div class="mainComment ">
     <div v-html="comment.text"></div>
     <div class="badge  badge-accent" style="display:inline;margin-right:20px">
       {{ Math.round(showTime.hour) }} hours
@@ -60,10 +61,9 @@ const showTime = computed(() => {
       hide replies
     </button>
     </div>
-    <div v-if=" comment.kids" v-show="showReply">
-      <div v-for="kid in comment.kids">
-        <CommentV1 :commentId="kid" />
-      </div>
+    </div>
+    <div v-if=" comment.kids" class="inC " v-show="showReply" v-for="kid in comment.kids">
+        <CommentV1  :commentId="kid" />
     </div>
   </div>
   <div v-else>
@@ -97,12 +97,22 @@ const showTime = computed(() => {
     -->
 </template>
 <style scoped>
-/*
-.innerComment{
-  margin:20px;
-  padding-bottom: 3px;
-  border-top: 1px solid rgb(201,203,204);
-  padding-top:8px;
+
+.comment{
+  border: 1px solid rgb(201,203,204);
+  display: grid;
+  grid-template-columns: repeat(20,1fr);
+  grid-template-rows: minx(10px,auto);
+  grid-gap: 10px;
+  margin: 0 auto;
 }
-*/
+
+.inC{
+  grid-column: 2/20;
+}
+.mainComment{
+  grid-column: 1/20;
+}
+
+
 </style>
